2baab62d8299756c3ddcd4c79c966ed2b10de734
[dyad] <riodedinheirorj@gmail.com>
2025-11-16 23:38:55 -0300

    [dyad] Resolvendo avisos do React Router adicionando future flags. - wrote 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -31,7 +31,7 @@ const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}> {/* Adicionado future flags */}
         <AuthEventBridge />
         <Routes>
           <Route path="/" element={<Index />} />
